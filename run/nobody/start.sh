6c7816603529aeb7fe5fde6186c7a9c43cd1b3f5
binhex <megalith01@gmail.com>
2023-02-06 10:59:59 +0000

    attempted fix for dotnet error

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -3,5 +3,8 @@
 # setup environment
 export XDG_CONFIG_HOME="/config/xdg"
 
+# fix for dotnet 2.1 error https://github.com/dotnet/runtime/issues/3168#issuecomment-389070397
+export TMPDIR=/tmp
+
 # run app
 /usr/lib/radarr/bin/Radarr -nobrowser -data=/config
